574e9b4962294a6b3ad385fe1c3f4908e832a906
LSM-HUE <2354796706@qq.com>
2026-03-23 11:12:03 +0800

    APP样式文件中新增ComboBox公共样式

--- Project/Project/obj/Debug/net10.0-windows/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net10.0-windows/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46271e2edbff8a70d4476fa3d53c84ec83f16dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fe68e0d15d1bb1129eec03f75db397bdaa5500")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
