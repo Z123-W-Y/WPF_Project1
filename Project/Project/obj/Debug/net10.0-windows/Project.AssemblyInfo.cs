3d92bd8ff76fd1a4a6774161c0687b3b3bb5cc74
LSM-HUE <2354796706@qq.com>
2026-03-23 09:48:03 +0800

    新增222.txt文件

--- Project/Project/obj/Debug/net10.0-windows/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net10.0-windows/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0465a0524a7d11777c9e6023c5dff4b1b710783f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
